327b802f66633f0605f65192bafe051657479de5
Jovan Popovic <jocapc@gmail.com>
2017-04-22 22:53:46 +0200

    Added parallel CRUD test
    New test runs 100 threads and exectes CRUD test in every thread.
    Verifies that none of the threads is aborted/failed.

--- Test/ProductCatalog/CQRSTest.cs
+++ Test/ProductCatalog/CQRSTest.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using System.Xml;
+using System.Diagnostics;
 
 namespace CQRS
 {
@@ -18,59 +19,91 @@ namespace CQRS
 
         public Scenario()
         {
-            mapper = new QueryMapper(Util.Settings.ConnectionString.Replace("Database=master;", "Database=ProductCatalogDemo;"));
-            command = new Command(Util.Settings.ConnectionString.Replace("Database=master;", "Database=ProductCatalogDemo;"));
         }
 
         [Fact]
+        public void PCRUD()
+        {
+            List<Task> tasks = new List<Task>();
+            // Start three tasks. 
+            for (int ctr = 0; ctr <= 100; ctr++)
+                tasks.Add(Task.Run(() => CRUD()));
+
+            Task.WaitAll(tasks.ToArray());
+
+            foreach (var t in tasks)
+            {
+                Assert.Equal(false, t.IsCanceled);
+                Assert.Equal(true, t.IsCompleted);
+                Assert.Equal(false, t.IsFaulted);
+            }
+        }
+
+        static readonly object lockIdentity = new object();
+        static int identity = 4;
+        [Fact]
         public async Task CRUD()
         {
-            int count = -1;
-            await mapper.ExecuteReader("select count(*) from Company where CompanyId = 4", reader => count = reader.GetInt32(0));
-            Assert.Equal(0, count);
-            await command.ExecuteNonQuery("insert into Company(companyId, Name) values(4,'MSFT')");
+            var mapper = new QueryMapper(Util.Settings.ConnectionString.Replace("Database=master;", "Database=ProductCatalogDemo;"));
+            var command = new Command(Util.Settings.ConnectionString.Replace("Database=master;", "Database=ProductCatalogDemo;"));
 
-            await mapper.ExecuteReader("select count(*) from Company where CompanyId = 4", reader => count = reader.GetInt32(0));
+            int ID = -1;
+            lock (lockIdentity)
+            {
+                ID = identity++;
+            }
+            string NAME = "MSFT" + ID;
+            string NAME2 = "MDCS" + ID;
+            string NAME3 = "Microsoft" + ID;
+            string NAME4 = "MS" + ID;
+            int count = -1;
+            await mapper.ExecuteReader("select count(*) from Company where CompanyId = "+ID, reader => count = reader.GetInt32(0));
+            Assert.Equal(0, count);
+            await command.ExecuteNonQuery("insert into Company(companyId, Name) values(" + ID + ", '" + NAME + "')");
+
+            await mapper.ExecuteReader("select count(*) from Company where CompanyId = " + ID, reader => count = reader.GetInt32(0));
             Assert.Equal(1, count);
 
             int? id = null;
             string name = null;
-            await mapper.ExecuteReader("select CompanyId, Name from Company where CompanyId = 4", 
+            await mapper.ExecuteReader("select CompanyId, Name from Company where CompanyId = " + ID, 
                         reader => { id = reader.GetInt32(0); name = reader.GetString(1); });
 
-            Assert.Equal(4, id);
-            Assert.Equal("MSFT", name);
+            Assert.Equal(ID, id);
+            Assert.Equal(NAME, name);
 
-            await command.ExecuteNonQuery("update Company set Name = 'MDCS' where CompanyId = 4");
+            await command.ExecuteNonQuery("update Company set Name = '"+NAME2+"' where CompanyId = " + ID);
 
-            await mapper.ExecuteReader("select Name from Company where CompanyId = 4",
+            await mapper.ExecuteReader("select Name from Company where CompanyId = " + ID,
                         reader => { name = reader.GetString(0); });
-            Assert.Equal("MDCS", name);
+            Assert.Equal(NAME2, name);
 
-            await command.ExecuteNonQuery("delete Company where CompanyId = 4");
+            await command.ExecuteNonQuery("delete Company where CompanyId = " + ID);
 
-            await mapper.ExecuteReader("select count(*) from Company where CompanyId = 4", reader => count = reader.GetInt32(0));
+            await mapper.ExecuteReader("select count(*) from Company where CompanyId = " + ID, reader => count = reader.GetInt32(0));
             Assert.Equal(0, count);
 
             id = null;
-            await command.ExecuteReader("insert into Company(Name) output inserted.CompanyId values('Microsoft')", reader => id = reader.GetInt32(0));
+            await command.ExecuteReader("insert into Company(Name) output inserted.CompanyId values('"+NAME3+"')", reader => id = reader.GetInt32(0));
 
             await mapper.ExecuteReader("select count(*) from Company where CompanyId = " + id, reader => count = reader.GetInt32(0));
             Assert.Equal(1, count);
 
             await mapper.ExecuteReader("select Name from Company where CompanyId = " + id, reader => name = reader.GetString(0));
-            Assert.Equal("Microsoft", name);
+            Assert.Equal(NAME3, name);
 
-            name = null;
-            await command.ExecuteReader("update Company SET Name = 'MS' output deleted.Name where CompanyId = " + id, reader => name = reader.GetString(0));
-            Assert.Equal("Microsoft", name);
+            string oldname = null;
+            string newname = null;
+            await command.ExecuteReader("update Company SET Name = '" + NAME4 + "' output deleted.Name, inserted.Name where CompanyId = " + id, reader => { oldname = reader.GetString(0); newname = reader.GetString(1); });
+            Assert.Equal(NAME3, oldname);
+            Assert.Equal(NAME4, newname);
 
             name = null;
             int deletedId = -1;
             await command.ExecuteReader("delete Company output deleted.CompanyId, deleted.Name where CompanyId = " + id, 
                 reader => { deletedId = reader.GetInt32(0); name = reader.GetString(1); });
 
-            Assert.Equal("MS", name);
+            Assert.Equal(NAME4, name);
             Assert.Equal(id, deletedId);
 
         }
