002837d82dd7e4f0e34cb75b6a0cfbee45cb3b14
Jovan Popovic <jocapc@gmail.com>
2017-12-19 22:47:48 +0100

    Moved to VS2017

--- Code/Properties/AssemblyInfo.cs
+++ Code/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Belgrade.SqlClient")]
-[assembly: AssemblyCopyright("Copyright © Jovan Popovic 2016")]
+[assembly: AssemblyCopyright("Copyright © Jovan Popovic 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
