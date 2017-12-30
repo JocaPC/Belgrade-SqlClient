6bd95287d3ae3e94a35236b14696f0c2bbc2c880
Jovan Popovic <jocapc@gmail.com>
2017-12-30 13:58:32 +0100

    Configured NuGet build
    Nuget package is created automatically

--- Code/Properties/AssemblyInfo.cs
+++ Code/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Belgrade.SqlClient")]
-[assembly: AssemblyCopyright("Copyright © Jovan Popovic 2017")]
+[assembly: AssemblyCopyright("Copyright © Jovan Popovic 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
